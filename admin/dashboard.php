34a615df0422d15855e53e63de8e88064d2e5ed7
RiGa7 <rishikamanoj61976@gmail.com>
2024-03-29 23:11:20 +0530

    add new files

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -1,3 +1,25 @@
+<?php
+
+include '../components/connect.php';
+if (isset($_COOKIE['tutor_id'])) {
+   $tutor_id = $_COOKIE['tutor_id'];
+} else {
+   $tutor_id = '';
+}
+
+$count_content = $conn->prepare("SELECT * FROM 'content' WHERE tutor_id = ?");
+$total_contents = $count_content->rowCount();
+
+$count_playlist = $conn->prepare("SELECT * FROM 'playlist' WHERE tutor_id = ?");
+$total_playlists = $count_playlist->rowCount();
+
+$count_like = $conn->prepare("SELECT * FROM 'like' WHERE tutor_id = ?");
+$total_likes = $count_like->rowCount();
+
+$count_comments = $conn->prepare("SELECT * FROM comments' WHERE tutor_id = ?");
+$total_pcomments = $count_comments->rowCount();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -7,7 +29,7 @@
    <title>Dashboard</title>
 
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
-   <link rel="stylesheet" href="../css/style.css">
+   <link rel="stylesheet" href="../css/admin_style.css">
 </head>
 
 <body>
@@ -16,13 +38,38 @@
    include '../components/admin_header.php';
    ?>
 
+   <div class="dashboard">
+      <h1 class="heading">Dashboard</h1>
+         <div class="box">
+            <h3>Welcome</h3>
+            <p><?=$fetch_profile['name'];?></p>
+            <a href="profile.php" class="btn">View Profile</a>
+         </div>
+         <div class="box">
+            <h3><?=$total_contents;?></h3>
+            <p>Contents Uploaded</p>
+            <a href="add_content.php" class="btn">Add Content</a>
+         </div>
+         </div>
+         <div class="box">
+            <h3><?=$total_playlists;?></h3>
+            <p>playlists Uploaded</p>
+            <a href="add_playlist.php" class="btn">Add Playlist</a>
+         </div>
+         </div>
+         <div class="box">
+            <h3><?=$total_likes;?></h3>
+            <p>Likes</p>
+            <a href="add_likes.php" class="btn">View Content</a>
+         </div>
+         </div>
+         <div class="box">
+            <h3><?=$total_comments;?></h3>
+            <p>Comments</p>
+            <a href="comments.php" class="btn">View Comments</a>
+         </div>
+   </div>
 
-   <h1>dchsik</h1>
-
-
-   <?php
-   include '../components/footer.php';
-   ?>
 
    <script src="../js/admin_script.js"></script>
 </body>
--- components/admin_header.php
+++ components/admin_header.php
@@ -5,12 +5,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>header</title>
+    <link rel="stylesheet" href="../css/admin_style.css">
 </head>
 
 <body>
     <header class="header">
 
-        <section class="flex">
+        <sectio0n class="flex">
 
             <a href="dashboard.php" class="logo">Admin</a>
 
@@ -27,37 +28,77 @@
             </div>
 
             <div class="profile">
-         <?php
+                <?php
+                $select_profile = $conn->prepare("SELECT * FROM `tutors` WHERE id = ?");
+
+                $select_profile->execute([$tutor_id]);
+                if ($select_profile->rowCount() >  0) {
+                    $fetcht_profile = $select_profile->fetch(PDO::FETCH_ASSOC);
+
+                ?>
+                    <img src="../uploaded_files/<?= $fetch_profile['image']; ?>" alt="">
+                    <h3><?= $fetch_profile['name'] ?></h3>
+                    <span><?= $fetch_profile['profession'] ?></span>
+                    <a href="profile.html" class="btn">View Profile</a>
+
+                    <div class="flex-btn">
+                        <a href="login.php" class="option-btn"></a>
+                        <a href="register.php" class="option-btn"></a>
+                    </div>
+
+                    <a href="../components/admin_logout.php" onclick="return confirm('Do you want to log out')" class="delete-btn">logout</a>
+                <?php
+                } else {
+                ?>
+                    <h3 style="text-align: center;">Login</h3>
+                    <div class="flex-btn">
+                        <a href="login.php" class="option-btn">Login</a>
+                        <a href="register.php" class="option-btn">Register</a>
+                    </div>
+                <?php } ?>
+            </div>
+            </section>
+    </header>
+
+
+    <div class="side-bar">
+
+        <div class="close-side-bar">
+            <i class="fas fa-times"></i>
+        </div>
+
+        <div class="profile">
+            <?php
             $select_profile = $conn->prepare("SELECT * FROM `tutors` WHERE id = ?");
             $select_profile->execute([$tutor_id]);
-            if($select_profile->rowCount() > 0){
-            $fetch_profile = $select_profile->fetch(PDO::FETCH_ASSOC);
-         ?>
-         <img src="../uploaded_files/<?= $fetch_profile['image']; ?>" alt="">
-         <h3><?= $fetch_profile['name']; ?></h3>
-         <span><?= $fetch_profile['profession']; ?></span>
-         <a href="profile.php" class="btn">view profile</a>
-         <div class="flex-btn">
-            <a href="login.php" class="option-btn">login</a>
-            <a href="register.php" class="option-btn">register</a>
-         </div>
-         <a href="../components/admin_logout.php" onclick="return confirm('logout from this website?');" class="delete-btn">logout</a>
-         <?php
-            }else{
-         ?>
-         <h3>please login or register</h3>
-          <div class="flex-btn">
-            <a href="login.php" class="option-btn">login</a>
-            <a href="register.php" class="option-btn">register</a>
-         </div>
-         <?php
+            if ($select_profile->rowCount() > 0) {
+                $fetch_profile = $select_profile->fetch(PDO::FETCH_ASSOC);
+            ?>
+                <img src="../uploaded_files/<?= $fetch_profile['image']; ?>" alt="">
+                <h3><?= $fetch_profile['name']; ?></h3>
+                <span><?= $fetch_profile['profession']; ?></span>
+                <a href="profile.php" class="btn">view profile</a>
+            <?php
+            } else {
+            ?>
+                <h3>please login or register</h3>
+                <div class="flex-btn">
+                    <a href="login.php" class="option-btn">login</a>
+                    <a href="register.php" class="option-btn">register</a>
+                </div>
+            <?php
             }
-         ?>
-      </div>
+            ?>
+        </div>
 
-        </section>
-
-    </header>
+        <nav class="navbar">
+            <a href="dashboard.php"><i class="fas fa-home"></i><span>Home</span></a>
+            <a href="playlists.php"><i class="fa-solid fa-bars-staggered"></i><span>Playlists</span></a>
+            <a href="contents.php"><i class="fas fa-graduation-cap"></i><span>Contents</span></a>
+            <a href="comments.php"><i class="fas fa-comment"></i><span>Comments</span></a>
+            <a href="../components/admin_logout.php" onclick="return confirm('Want to logout?');"><i class="fas fa-right-from-bracket"></i><span>Logout</span></a>
+        </nav>
+    </div>
 </body>
 
 </html>
