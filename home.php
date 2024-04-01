cb9ac1d295b5b8ba5ad2c92ea294f6152e34c5e1
AV2897 <aasthavaish143@gmail.com>
2024-04-01 19:29:42 +0530

    hgsdgf

---
+++ home.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+   <link rel="stylesheet" href="../css/admin_style.css">
+</head>
+<body>
+    <?php include 'components/user_header.php'?>
+
+
+
+    <?php include 'components/footer.php'?>
+<script src = "js/script.js"></script>
+</body>
+</html>
