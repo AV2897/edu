cb963f0401464d7f7ef938bd3c75496a3c95b83d
AV2897 <aasthavaish143@gmail.com>
2024-04-04 18:49:09 +0530

    jfh

--- update.php
+++ update.php
@@ -2,14 +2,14 @@
 
 include 'components/connect.php';
 
-if (isset($_COOKIE['user_id'])) {
+if(isset($_COOKIE['user_id'])){
    $user_id = $_COOKIE['user_id'];
-} else {
+}else{
    $user_id = '';
    header('location:login.php');
 }
 
-if (isset($_POST['submit'])) {
+if(isset($_POST['submit'])){
 
    $select_user = $conn->prepare("SELECT * FROM `users` WHERE id = ? LIMIT 1");
    $select_user->execute([$user_id]);
@@ -21,21 +21,21 @@ if (isset($_POST['submit'])) {
    $name = $_POST['name'];
    $name = filter_var($name, FILTER_SANITIZE_STRING);
 
-   if (!empty($name)) {
-      $update_name = $conn->prepare("UPDATE `users` SET name = ? WHERE id = ?");
-      $update_name->execute([$name, $user_id]);
-      $message[] = 'username updated successfully!';
-   }
+  if(!empty($name)){
+   $update_name = $conn->prepare("UPDATE `users` SET name = ? WHERE id = ?");
+   $update_name->execute([$name, $user_id]);
+   $message[] = 'username updated successfully!';
+  }
 
    $email = $_POST['email'];
    $email = filter_var($email, FILTER_SANITIZE_STRING);
 
-   if (!empty($email)) {
+   if(!empty($email)){
       $select_email = $conn->prepare("SELECT email FROM `users` WHERE email = ?");
       $select_email->execute([$email]);
-      if ($select_email->rowCount() > 0) {
+      if($select_email->rowCount() > 0){
          $message[] = 'email already taken!';
-      } else {
+      }else{
          $update_email = $conn->prepare("UPDATE `users` SET email = ? WHERE id = ?");
          $update_email->execute([$email, $user_id]);
          $message[] = 'email updated successfully!';
@@ -45,20 +45,20 @@ if (isset($_POST['submit'])) {
    $image = $_FILES['image']['name'];
    $image = filter_var($image, FILTER_SANITIZE_STRING);
    $ext = pathinfo($image, PATHINFO_EXTENSION);
-   $rename = unique_id() . '.' . $ext;
+   $rename = unique_id().'.'.$ext;
    $image_size = $_FILES['image']['size'];
    $image_tmp_name = $_FILES['image']['tmp_name'];
-   $image_folder = 'uploaded_files/' . $rename;
+   $image_folder = 'uploaded_files/'.$rename;
 
-   if (!empty($image)) {
-      if ($image_size > 2000000) {
+   if(!empty($image)){
+      if($image_size > 2000000){
          $message[] = 'image size too large!';
-      } else {
+      }else{
          $update_image = $conn->prepare("UPDATE `users` SET `image` = ? WHERE id = ?");
          $update_image->execute([$rename, $user_id]);
          move_uploaded_file($image_tmp_name, $image_folder);
-         if ($prev_image != '' and $prev_image != $rename) {
-            unlink('uploaded_files/' . $prev_image);
+         if($prev_image != '' AND $prev_image != $rename){
+            unlink('uploaded_files/'.$prev_image);
          }
          $message[] = 'image updated successfully!';
       }
@@ -72,73 +72,77 @@ if (isset($_POST['submit'])) {
    $cpass = sha1($_POST['cpass']);
    $cpass = filter_var($cpass, FILTER_SANITIZE_STRING);
 
-   if ($old_pass != $empty_pass) {
-      if ($old_pass != $prev_pass) {
+   if($old_pass != $empty_pass){
+      if($old_pass != $prev_pass){
          $message[] = 'old password not matched!';
-      } elseif ($new_pass != $cpass) {
+      }elseif($new_pass != $cpass){
          $message[] = 'confirm password not matched!';
-      } else {
-         if ($new_pass != $empty_pass) {
+      }else{
+         if($new_pass != $empty_pass){
             $update_pass = $conn->prepare("UPDATE `users` SET password = ? WHERE id = ?");
             $update_pass->execute([$cpass, $user_id]);
             $message[] = 'password updated successfully!';
-         } else {
+         }else{
             $message[] = 'please enter a new password!';
          }
       }
    }
+
 }
 
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>Update Profile</title>
+   <title>update profile</title>
 
-   <!-- Font Awesome CDN link -->
+   <!-- font awesome cdn link  -->
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
 
-   <!-- Custom CSS file link -->
+   <!-- custom css file link  -->
    <link rel="stylesheet" href="css/style.css">
 
 </head>
-
 <body>
-   <?php include 'components/user_header.php'; ?>
-   <section class="form-container">
 
-      <form action="" method="post" enctype="multipart/form-data">
-         <h3>Update Profile</h3>
-         <div class="flex">
-            <div class="col">
-               <p>Your Name</p>
-               <input type="text" name="name" placeholder="<?= $fetch_profile['name']; ?>" maxlength="100" class="box">
-               <p>Your Email</p>
-               <input type="email" name="email" placeholder="<?= $fetch_profile['email']; ?>" maxlength="100" class="box">
-               <p>Update Profile Picture</p>
-               <input type="file" name="image" accept="image/*" class="box">
-            </div>
-            <div class="col">
-               <p>Old Password</p>
-               <input type="password" name="old_pass" placeholder="Enter your old password" maxlength="50" class="box">
-               <p>New Password</p>
-               <input type="password" name="new_pass" placeholder="Enter your new password" maxlength="50" class="box">
-               <p>Confirm Password</p>
-               <input type="password" name="cpass" placeholder="Confirm your new password" maxlength="50" class="box">
-            </div>
+<?php include 'components/user_header.php'; ?>
+
+<section class="form-container" style="min-height: calc(100vh - 19rem);">
+
+   <form action="" method="post" enctype="multipart/form-data">
+      <h3>update profile</h3>
+      <div class="flex">
+         <div class="col">
+            <p>your name</p>
+            <input type="text" name="name" placeholder="<?= $fetch_profile['name']; ?>" maxlength="100" class="box">
+            <p>your email</p>
+            <input type="email" name="email" placeholder="<?= $fetch_profile['email']; ?>" maxlength="100" class="box">
+            <p>update pic</p>
+            <input type="file" name="image" accept="image/*" class="box">
          </div>
-         <input type="submit" name="submit" value="Update Profile" class="btn">
-      </form>
+         <div class="col">
+               <p>old password</p>
+               <input type="password" name="old_pass" placeholder="enter your old password" maxlength="50" class="box">
+               <p>new password</p>
+               <input type="password" name="new_pass" placeholder="enter your new password" maxlength="50" class="box">
+               <p>confirm password</p>
+               <input type="password" name="cpass" placeholder="confirm your new password" maxlength="50" class="box">
+         </div>
+      </div>
+      <input type="submit" name="submit" value="update profile" class="btn">
+   </form>
 
-   </section>
+</section>
 
-   <!-- Custom JS file link -->
-   <script src="js/script.js"></script>
+<!-- update profile section ends -->
 
+
+<!-- custom js file link  -->
+<script src="js/script.js"></script>
+   
 </body>
-
 </html>
